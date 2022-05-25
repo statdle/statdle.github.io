a6d452377f239aae7fbf568853a87e7645de1235
9ps <9ps@pm.me>
2022-05-25 23:14:50 +1000

    modalhow update

--- src/components/ModalHow.js
+++ src/components/ModalHow.js
@@ -67,20 +67,19 @@ class ModalHow extends React.Component {
               Find the Secret Nation-<em>State</em> using <em>Stat</em>istics! A new game everyday!
             </h1>
             <p className="mag-top">
-              Guessing a country will show its rank on 4 random catagories, and
-              how it compares to the <em> Secret Country</em>.
+            You are given 4 random "stats" that will help you figure out what the hidden country is. Guessing a country will reveal it's rank among each stat in relation to the hidden country.
             </p>
             {exampleOne}
             <ul>
               <li>
-                <em>Austria</em> is above the <em> Secret Country</em>, while <em> San Marino</em> is below.
+                <em>Austria</em> is rank #14, above the <em>Secret Country</em>, while <em>San Marino</em> is below at rank #129. Hence the <em>Secret Country</em> is between #14 and #129. Further guesses will reveal more information and eliminate more possibilities.
               </li>
             </ul>
             {exampleTwo}
             <ul>
               <li>
                 A new guess, like <em>Zimbabwe</em> is lower than
-                <em> San Marino</em> and the <em> Secret Country</em>
+                both <em>San Marino</em> and the <em>Secret Country</em>, displayed with a line.
               </li>
 
               <a className="item-dark" href="https://docs.google.com/forms/d/e/1FAIpQLSf9NfB5E7mMjUAhYh-GrwS8uS1s3jZRQQ9dAP8_DB4OKmU16w/viewform?usp=sf_link">Form for feedback or suggestions</a>
